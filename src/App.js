c37e0c85660ffe308cdc0fdf71f72c8935384e88
conbec <56614504+conbec@users.noreply.github.com>
2020-01-22 15:57:46 -0800

    result of pairing in class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const ColorSlider = (props) => {
+  const { text, value, setValue } = props
+  return (
+    <div>
+      <input value={value} type='range' min='0' max='255' step='1' onChange={ev => setValue(ev.target.value)} /> {text} {value}
+    </div>
+  );
+}
+
 
 function App() {
+  const [red, setRed] = useState('125');
+  const [green, setGreen] = useState('125');
+  const [blue, setBlue] = useState('125');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        <ColorSlider text='Red' value={red} setValue={setRed} />
+        <ColorSlider text='Green' value={green} setValue={setGreen} />
+        <ColorSlider text='Blue' value={blue} setValue={setBlue} />
+      </div>
+      <div className='swatch' style={{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }}>
+      </div>
+    </main>
   );
 }
 
